51d0c4fabdfb68884e3d99198427129e56d652fa
Nolan Carlisle <143767678+NCarlisle24@users.noreply.github.com>
2025-06-11 16:42:02 -1000

    added noise files

--- include/mcc-utils/math/noise.hpp
+++ include/mcc-utils/math/noise.hpp
@@ -1,8 +1,14 @@
 #ifndef NOISE_HPP
 #define NOISE_HPP
 
-#include <cmath>
-#include <random>
+#include <ctime>
+#include <cstdlib>
 #include <iostream>
+
+inline void initializeRandomSeed() {
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+}
+
+void perlinNoise(void* const* const data, const unsigned int &width, const unsigned int &height, const float &scale);
 
 #endif
---
+++ src/mcc-utils/math/noise.cpp
@@ -0,0 +1,5 @@
+#include <mcc-utils/math/noise.hpp>
+
+void perlinNoise(void* const* const data, const unsigned int &width, const unsigned int &height, const float &scale) {
+    
+}
